7995f1439cfa237c4410b073355153bbae7a30cd
georgianabratucu <bratucu_georgiana@yahoo.com>
2019-04-13 12:23:03 +0300

    implement calculateAge and add interest

--- src/main/java/com/cegekaacademy/model/DepositAccount.java
+++ src/main/java/com/cegekaacademy/model/DepositAccount.java
@@ -2,7 +2,31 @@ package com.cegekaacademy.model;
 
 public class DepositAccount extends BankAccount {
 
-    //TODO add interest
+    private double interestPercent;
+    private int durationInMonth;
+
+
+    public DepositAccount(String iban, double balance, double interestPercent, int durationInMonth) {
+        super(iban, balance);
+        this.interestPercent = interestPercent;
+        this.durationInMonth = durationInMonth;
+    }
+
+    public double getInterestPercent() {
+        return interestPercent;
+    }
+
+    public void setInterestPercent(double interestProcent) {
+        this.interestPercent = interestProcent;
+    }
+
+    public int getDurationInMonth() {
+        return durationInMonth;
+    }
+
+    public void setDurationInMonth(int durationInMonth) {
+        this.durationInMonth = durationInMonth;
+    }
 
     public DepositAccount(String iban, double balance) {
         super(iban, balance);
@@ -16,5 +40,21 @@ public class DepositAccount extends BankAccount {
         }
 
         return super.deposit(amount);
+    }
+
+    public double getInterest(){
+        if(getBalance()<0 && interestPercent <0)
+            return 0;
+
+     return (this.getBalance()*interestPercent*durationInMonth/12);
+    }
+    public double getInterestAfterTaxDeduction(){
+        double interest=getInterest();
+        return interest-0.01*interest;
+    }
+
+    public void addInterest(){
+
+        this.setBalance(this.getBalance()+getInterestAfterTaxDeduction());
     }
 }
--- src/main/java/com/cegekaacademy/model/Person.java
+++ src/main/java/com/cegekaacademy/model/Person.java
@@ -1,5 +1,8 @@
 package com.cegekaacademy.model;
 
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
 public class Person {
 
     private String name;
@@ -40,8 +43,25 @@ public class Person {
     }
 
     public int calculateAge() {
-        // TODO implement me
 
-        return 0;
+        int year = 0;
+        if (pid.charAt(0) == '1' || pid.charAt(0) == '2')
+            year = (1900 + Integer.parseInt("" + pid.charAt(1) + pid.charAt(2)));
+        if (pid.charAt(0) == '3' || pid.charAt(0) == '4')
+            year = (1800 + Integer.parseInt("" + pid.charAt(1) + pid.charAt(2)));
+        if (pid.charAt(0) == '5' || pid.charAt(0) == '6')
+            year = (2000 + Integer.parseInt("" + pid.charAt(1) + pid.charAt(2)));
+
+        int month = Integer.parseInt("" + pid.charAt(3) + pid.charAt(4));
+        int day = Integer.parseInt("" + pid.charAt(5) + pid.charAt(6));
+
+        Calendar birthDate = Calendar.getInstance();
+        birthDate.set(year, month, day);
+        Calendar currentDate = Calendar.getInstance();
+        long ageInDays = TimeUnit.MILLISECONDS
+                .toDays(Math.abs(currentDate.getTimeInMillis() - birthDate.getTimeInMillis()));
+        return (int) (ageInDays / 365);
+
     }
+
 }
