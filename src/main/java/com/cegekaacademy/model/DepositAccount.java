47794a6b937dccf781268335be9ad1a8586b8088
iulianab <iuliana.bratucu.97@gmail.com>
2019-04-13 12:24:09 +0300

    implement calculateAge and modify DepositAccount class

--- src/main/java/com/cegekaacademy/model/DepositAccount.java
+++ src/main/java/com/cegekaacademy/model/DepositAccount.java
@@ -2,10 +2,17 @@ package com.cegekaacademy.model;
 
 public class DepositAccount extends BankAccount {
 
-    //TODO add interest
+    private float interestRate;
+    private int depositPeriod;
 
     public DepositAccount(String iban, double balance) {
         super(iban, balance);
+    }
+
+    public DepositAccount(String iban, double balance, float interestRate, int depositPeriod) {
+        super(iban, balance);
+        this.interestRate = interestRate;
+        this.depositPeriod = depositPeriod;
     }
 
     @Override
@@ -15,5 +22,35 @@ public class DepositAccount extends BankAccount {
         }
 
         return super.deposit(amount);
+    }
+
+    public float getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(float interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public int getDepositPeriod() {
+        return depositPeriod;
+    }
+
+    public void setDepositPeriod(int depositPeriod) {
+        this.depositPeriod = depositPeriod;
+    }
+
+    public double calculateRate(){
+
+        if(this.getBalance() <= 0 || this.interestRate <= 0){
+            return 0;
+        }
+
+        return this.getBalance() * this.interestRate * this.depositPeriod / 12;
+    }
+
+    public void addInterestToBalance(){
+
+        this.setBalance(this.getBalance() + this.calculateRate());
     }
 }
