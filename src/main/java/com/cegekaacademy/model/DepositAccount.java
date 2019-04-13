d908defd20054553a431331cd3c4530d408db79e
mihaela <bernat_andreeamihaela@yahoo.com>
2019-04-14 00:02:50 +0300

    add interest

--- src/main/java/com/cegekaacademy/model/DepositAccount.java
+++ src/main/java/com/cegekaacademy/model/DepositAccount.java
@@ -2,10 +2,29 @@ package com.cegekaacademy.model;
 
 public class DepositAccount extends BankAccount {
 
-    //TODO add interest
+    private double interest;
+    private int periodOfTime;
 
-    public DepositAccount(String iban, double balance) {
+    public DepositAccount(String iban, double balance, double interest, int periodOfTime) {
         super(iban, balance);
+        this.interest = interest;
+        this.periodOfTime = periodOfTime;
+    }
+
+    public double getInterest() {
+        return interest;
+    }
+
+    public void setInterest(double interest) {
+        this.interest = interest;
+    }
+
+    public int getPeriodOfTime() {
+        return periodOfTime;
+    }
+
+    public void setPeriodOfTime(int periodOfTime) {
+        this.periodOfTime = periodOfTime;
     }
 
     @Override
@@ -15,5 +34,18 @@ public class DepositAccount extends BankAccount {
         }
 
         return super.deposit(amount);
+    }
+
+    public double getRate(){
+        if(this.getBalance() <= 0 || this.interest<=0 || this.periodOfTime <= 0){
+            return 0;
+        }
+
+        return this.getBalance()*this.interest*this.periodOfTime/12;
+    }
+
+    public void addInterestRate(){
+
+        this.setBalance(this.getRate() + this.getBalance());
     }
 }
