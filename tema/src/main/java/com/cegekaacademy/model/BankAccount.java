b758fd01adf2f2e7c60ec9e15b84f67c5ab33562
Bianca Virtopeanu <bianca-georgiana.virtopeanu@ing.ro>
2019-04-15 11:06:50 +0300

    Revert "Revert "tema""
    This reverts commit eb09b94

---
+++ tema/src/main/java/com/cegekaacademy/model/BankAccount.java
@@ -0,0 +1,55 @@
+package com.cegekaacademy.model;
+
+public abstract class BankAccount {
+
+    private String iban;
+    private double balance;
+
+    public BankAccount(String iban, double balance) {
+        this.iban = iban;
+        this.balance = balance;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount <= 0 || amount > balance) {
+            return false;
+        }
+        this.balance = this.balance - amount;
+        return true;
+    }
+
+    public boolean deposit(double amount) {
+        if (amount <= 0) {
+            return false;
+        }
+
+        this.balance = this.balance + amount;
+        return true;
+    }
+
+    public boolean transfer(BankAccount destination, double amount) {
+        if(this.balance>=amount && amount>0 && destination!=null)
+        {
+            this.balance-=amount;
+            destination.balance+=amount;
+            return true;
+        }
+        return false;
+    }
+}
