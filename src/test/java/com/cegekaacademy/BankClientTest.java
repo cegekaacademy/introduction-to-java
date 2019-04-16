511dc7c23a1ffc431eb785701655c3b3843088cb
Cosmin Alex <silvian1995@yahoo.com>
2019-04-16 15:00:26 +0300

    Add test for total balance

--- src/test/java/com/cegekaacademy/BankClientTest.java
+++ src/test/java/com/cegekaacademy/BankClientTest.java
@@ -4,6 +4,7 @@ package com.cegekaacademy;
 import com.cegekaacademy.bank.BankClient;
 import com.cegekaacademy.model.BankAccount;
 import com.cegekaacademy.model.CurrentAccount;
+import com.cegekaacademy.model.DepositAccount;
 import com.cegekaacademy.model.Person;
 import org.junit.Assert;
 import org.junit.Test;
@@ -39,5 +40,27 @@ public class BankClientTest {
 
         Mockito.verify(person, Mockito.times(1)).calculateAge();
         //Mockito.verify(currentAccount, Mockito.times(1)).getBalance();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void GIVEN_noAccountsAndNoPerson_WHEN_getTotalBalance_THEN_returnThrowException() {
+        BankClient bankClient = new BankClient(null, null);
+        bankClient.calculateSeniorityBonus();
+    }
+
+    @Test
+    public void GIVEN_accountsAndPersonValid_WHEN_getTotalBalance_THEN_returnBonus() {
+        CurrentAccount currentAccount = Mockito.mock(CurrentAccount.class);
+        DepositAccount depositAccount = Mockito.mock(DepositAccount.class);
+        List<BankAccount> bankAccounts = new ArrayList<>();
+        bankAccounts.add(currentAccount);
+        bankAccounts.add(depositAccount);
+        Person person = Mockito.mock(Person.class);
+        Mockito.when(bankAccounts.get(0).getBalance()).thenReturn(100D);
+        Mockito.when(bankAccounts.get(1).getBalance()).thenReturn(200D);
+        BankClient bankClient = new BankClient(person, bankAccounts);
+        Assert.assertEquals(300D, bankClient.getTotalBalance(), 0);
+        Mockito.verify(currentAccount, Mockito.times(1)).getBalance();
+        Mockito.verify(depositAccount, Mockito.times(1)).getBalance();
     }
 }
